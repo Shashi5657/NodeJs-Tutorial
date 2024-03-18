7d447d12ecca5f712651cfba5abc66bd1b001905
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-18 22:05:39 +0530

    hh

--- 17-http-stream.js
+++ 17-http-stream.js
@@ -5,11 +5,11 @@ http
   .createServer(function (req, res) {
     // const text = fs.readFileSync('./content/big.txt', 'utf-8')
     // res.end(text)
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf-8');
-    fileStream.on('open', () => {
-      fileStream.pipe(res)
+    const fileStream = fs.createReadStream("./content/big.txt", "utf-8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
     });
-    fileStream.on('error', (err) => {
+    fileStream.on("error", (err) => {
       res.end(err);
     });
   })
