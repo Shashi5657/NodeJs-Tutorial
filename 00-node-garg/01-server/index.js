5524a9bbfbf9b3bf95af4ea903b94446ce5d8128
Shashi5657 <sangepushashidhar@gmail.com>
2024-04-07 23:46:17 +0530

    url params

--- 00-node-garg/01-server/index.js
+++ 00-node-garg/01-server/index.js
@@ -1,15 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
   console.log("req recieved");
-  const log = `${Date.now()}: New req recieved \n`;
+  if(req.url === "/favicon.ico") return res.end()
+  const log = `${Date.now()}: ${req.url} New req recieved \n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch(req.url){
-        case '/' : res.end('Home Page'); break;
-        case '/about' : res.end('Myself Shashii'); break;
-        case '/skills' : res.end('React & Node'); break;
-        default : res.end('error, page not found')
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        const userName = myUrl.query.myname
+        res.end(`hi ${userName}`);
+        break;
+      case "/skills":
+        res.end("React & Node");
+        break;
+        case "/search" : 
+        const search = myUrl.query.search_query;
+        res.end("result for" + search)
+      default:
+        res.end("error, page not found");
     }
   });
 });
