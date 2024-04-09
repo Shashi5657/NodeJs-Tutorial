9783684f492c9522d58141d8932a3e4a19aa1078
Shashi5657 <sangepushashidhar@gmail.com>
2024-04-09 22:34:36 +0530

    new project on nodejs

--- 00-node-garg/02-project/index.js
+++ 00-node-garg/02-project/index.js
@@ -1,8 +1,42 @@
-const express = require('express')
+const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 //Routes
 
-app.listen(PORT, ()=> {console.log(`Server started on ${PORT}`)})
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+  res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((rew, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
