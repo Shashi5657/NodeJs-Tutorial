12be322bc6325e684e3f309c8b60a78b8aac8d37
Shashi5657 <sangepushashidhar@gmail.com>
2024-04-10 11:52:30 +0530

    new project on nodejs

--- 00-node-garg/02-project/index.js
+++ 00-node-garg/02-project/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require('fs')
 
 const app = express();
 const PORT = 8000;
+
+//middleware
+app.use(express.urlencoded({extended : false}))
 
 //Routes
 
@@ -34,7 +38,12 @@ app
   });
 
 app.post("/api/users", (req, res) => {
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({...body, id : users.length + 1});
+  fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users), (err, response)=> {
+    return res.json({ status: "success", id : users.length });
+  })
+  
 });
 
 app.listen(PORT, () => {
